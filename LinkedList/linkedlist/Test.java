ffc22303b1836bc41addddb9de5ed848fc07d42d
W839740052 <yinghaowang95@outlook.com>
2020-02-05 00:03:00 -0500

    Add files via upload

---
+++ LinkedList/linkedlist/Test.java
@@ -0,0 +1,26 @@
+package com.wyh.linkedlist;
+
+public class Test {
+
+	public static void main(String[] args) {
+		LinkedList<Integer> linkedlist =new LinkedList<>();
+		for(int i=0;i<5;i++) {
+			linkedlist.addFirst(i);
+			System.out.println(linkedlist);
+		}
+		linkedlist.add(2, 666);
+		System.out.println(linkedlist);
+		
+		
+		linkedlist.remove(2);
+		System.out.println(linkedlist);
+		
+		linkedlist.removeFirst();
+		System.out.println(linkedlist);
+		
+		linkedlist.removeLast();
+		System.out.println(linkedlist);
+
+	}
+
+}
